66f6385b9a374c2501838f94e3d312fba9d2313f
emeric-francisod <emeric.francisod@gmail.com>
2020-05-09 17:39:09 +0200

    Le lien apparaît et reste au clic sur le lien. Disparait au clic sur la fenêtre

---
+++ script.js
@@ -0,0 +1,22 @@
+let moreElts = document.getElementsByClassName("more");
+
+for (let i = 0 ; i < moreElts.length ; i++) {
+    moreElts[i].addEventListener("click", function(e) {
+        console.log("clic sur le lien");
+        e.preventDefault();
+        e.currentTarget.classList.toggle("opened");
+    });
+}
+
+window.addEventListener("click", function(e) {
+    console.log("clic sur la fenêtre");
+    let openedMenuElts = document.querySelectorAll(".more.opened ul.submenu");
+    for (let i = 0 ; i < openedMenuElts.length ; i++) {
+        if (e.pageY < openedMenuElts[i].offsetTop
+            ||e.pageY > openedMenuElts[i].offsetTop + openedMenuElts[i].offsetheight
+            || e.pageX < openedMenuElts[i].offsetLeft
+            ||e.pageX > openedMenuElts[i].offsetLeft + openedMenuElts[i].offsetWidth) {
+                openedMenuElts[i].parentNode.classList.toggle("opened");
+            }
+    }
+}, false);
