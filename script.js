b96e546bc9ceece0b615c4c1b8ae9850c9304617
emeric-francisod <emeric.francisod@gmail.com>
2020-05-10 08:39:56 +0200

    Ouverture et fermeture maintenant dans une fonction

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let moreElts = document.getElementsByClassName("more");
 for (let i = 0 ; i < moreElts.length ; i++) {
     moreElts[i].addEventListener("click", function(e) {
         e.preventDefault();
-        e.currentTarget.classList.toggle("opened");
+        toggleMenu(e.currentTarget);
     });
 }
 
@@ -24,7 +24,7 @@ window.addEventListener("click", function(e) {
             ||e.pageY > openedMenuElts[i].offsetTop + openedMenuElts[i].offsetheight
             || e.pageX < openedMenuElts[i].offsetLeft
             ||e.pageX > openedMenuElts[i].offsetLeft + openedMenuElts[i].offsetWidth) {
-                openedMenuElts[i].parentNode.classList.toggle("opened");
+                toggleMenu(openedMenuElts[i].parentNode);
             }
     }
 }, true);
@@ -33,6 +33,6 @@ window.addEventListener("scroll", function(e) {
     let openedMenuElts = document.querySelectorAll(".more.opened ul.submenu");
     for (let i = 0 ; i < openedMenuElts.length ; i++) {
         e.stopPropagation();
-        openedMenuElts[i].parentNode.classList.toggle("opened");
+        toggleMenu(openedMenuElts[i].parentNode);
     }
 });
