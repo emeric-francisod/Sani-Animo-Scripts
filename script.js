c96ee76d4683ca34e7effc5c0bb29c4744de12f7
emeric-francisod <emeric.francisod@gmail.com>
2020-05-12 17:57:49 +0200

    Modidfication gestion événements clics

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function toggleMenu (plusLinkElt, action = "hey!") {
-    console.log(action);
     if (plusLinkElt.classList.contains("opened") && action !== "open" || action === "close") {
         console.log("Menu closing");
         plusLinkElt.classList.remove("opened");
@@ -27,25 +26,32 @@ let moreElts = document.getElementsByClassName("more");
 
 for (let i = 0 ; i < moreElts.length ; i++) {
     moreElts[i].addEventListener("click", function(e) {
+        console.log("Link click")
         e.preventDefault();
         toggleMenu(e.currentTarget);
     });
 
-    /* moreElts[i].querySelector("a:first-child").addEventListener("focus", function(e) {
+    /*moreElts[i].querySelector("a:first-child").addEventListener("focus", function(e) {
         e.preventDefault();
         toggleMenu(e.currentTarget.parentNode, "open");
     }); */
 }
 
 window.addEventListener("click", function(e) {
+    console.log("Window click");
     let openedMenuElts = document.querySelectorAll(".more.opened ul.submenu");
     for (let i = 0 ; i < openedMenuElts.length ; i++) {
         e.preventDefault();
-        e.stopPropagation();
-        if (e.pageY < openedMenuElts[i].offsetTop
+        // e.stopPropagation();
+        let plusMenuElt = openedMenuElts[i].parentNode;
+        if ((e.pageY < openedMenuElts[i].offsetTop
             ||e.pageY > openedMenuElts[i].offsetTop + openedMenuElts[i].offsetHeight
             ||e.pageX < openedMenuElts[i].offsetLeft
-            ||e.pageX > openedMenuElts[i].offsetLeft + openedMenuElts[i].offsetWidth) {
+            ||e.pageX > openedMenuElts[i].offsetLeft + openedMenuElts[i].offsetWidth)
+            && (e.pageY < plusMenuElt.offsetTop
+            ||e.pageY > plusMenuElt.offsetTop + plusMenuElt.offsetHeight
+            ||e.pageX < plusMenuElt.offsetLeft
+            ||e.pageX > plusMenuElt.offsetLeft + plusMenuElt.offsetWidth)) {
                 toggleMenu(openedMenuElts[i].parentNode, "window click");
             }
     }
