2ec4069bc81920df94f9f588553e69eab6a05098
emeric-francisod <emeric.francisod@gmail.com>
2020-05-13 17:38:42 +0200

    Retire le clic, seuls les événements blur et focus sur le lien

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ function toggleMenu (plusLinkElt, action = "hey!") {
     } else if (!plusLinkElt.classList.contains("opened") || action === "open"){
         console.log("Menu opening");
         plusLinkElt.classList.add("opened");
-        // plusLinkElt.querySelector(".submenu li:first-child a").focus();
-
     }
 
 }
@@ -25,23 +23,28 @@ function windowEventMenuCallback (e) {
 let moreElts = document.getElementsByClassName("more");
 
 for (let i = 0 ; i < moreElts.length ; i++) {
-    moreElts[i].addEventListener("click", function(e) {
+    /* moreElts[i].addEventListener("click", function(e) {
         console.log("Link click")
         e.preventDefault();
         toggleMenu(e.currentTarget);
-    });
+    }); */
 
     /* moreElts[i].addEventListener("focusout", function(e) {
         console.log("Focus out");
         toggleMenu(e.currentTarget, "close");
     }); */
 
-    /* moreElts[i].querySelector("a:first-child").addEventListener("focus", function(e) {
+    moreElts[i].querySelector("a:first-child").addEventListener("focus", function(e) {
         console.log("Menu focus");
-        e.stopImmediatePropagation();
         e.preventDefault();
         toggleMenu(e.currentTarget.parentNode, "open");
-    }); */
+    });
+
+    moreElts[i].querySelector("a:first-child").addEventListener("blur", function(e) {
+        console.log("Menu blur");
+        e.preventDefault();
+        toggleMenu(e.currentTarget.parentNode, "close");
+    });
 }
 
 window.addEventListener("scroll", windowEventMenuCallback);
