7c85c018b1d4a7b9072704ce0df15c550c007ed8
emeric-francisod <emeric.francisod@gmail.com>
2020-05-20 18:59:47 +0200

    Ouverture du popin au clic

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let popinOpenersElts = document.getElementsByClassName("popin-opener");
 
 for (let i = 0 ; i < popinOpenersElts.length ; i++) {
     popinOpenersElts[i].addEventListener("click", function(e) {
-        console.log(e.currentTarget.dataset.popinTarget);
+        let popinElt = document.getElementById(e.currentTarget.dataset.popinTarget);
+        popinElt.classList.add("opened");
+        document.body.classList.add("popin-opened");
     });
 }
