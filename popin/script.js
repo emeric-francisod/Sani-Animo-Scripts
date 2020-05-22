eaa45764c7efdf80133cb3a493225621f18f3932
emeric-francisod <emeric.francisod@gmail.com>
2020-05-22 11:26:00 +0200

    Préparation de la page pour la gestion des tabulations. Recherche des éléments tabulables.

--- popin/script.js
+++ popin/script.js
@@ -8,9 +8,34 @@ function closePopin() {
 
 
 
+function getTabElements(parentElt) {
+    let query = "";
+
+    for (let i = 0 ; i < FOCUS_ELT.length ; i++) {
+        query += FOCUS_ELT[i] + ", ";
+    }
+    for (let i = 0 ; i < FOCUS_ATTR.length ; i++) {
+        query += "[" + FOCUS_ATTR[i] + "]";
+        if (i < FOCUS_ATTR.length - 1) {
+            query += ", ";
+        }
+    }
+
+    tabElts = parentElt.querySelectorAll(query);
+    console.log(tabElts);
+}
+
+
+
+const FOCUS_ELT = ["a", "area", "audio", "button", "embed", "form", "iframe", "img", "input", "keygen", "label", "object", "select", "svg", "textarea", "video"];
+const FOCUS_ATTR = ["contenteditable", "focusable", "tabindex"];
+
 let popinOpenersElts = document.getElementsByClassName("popin-opener");
 let popinCloseButtonElts = document.getElementsByClassName("popin-close");
 let blurElt = document.getElementById("blur");
+let contentElt = document.getElementById("content");
+
+getTabElements(contentElt);
 
 for (let i = 0 ; i < popinOpenersElts.length ; i++) {
     popinOpenersElts[i].addEventListener("click", function(e) {
