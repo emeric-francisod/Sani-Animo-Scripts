03434759a8d00731c4b80290d3f3663c629c1c4a
emeric-francisod <emeric.francisod@gmail.com>
2020-05-27 16:36:39 +0200

    Adaptation du script et du html pour le menu

--- Menu/script.js
+++ Menu/script.js
@@ -1,3 +1,62 @@
+(function () {
+    function hamburgerMenuToggle (buttonElt) {
+        if (menuElt.classList.contains("close")) {
+            hamburgerMenuClose();
+        } else {
+            hamburgerMenuOpen(buttonElt);
+        }
+    }
+
+    function hamburgerMenuClose () {
+        let openedMenuElt = document.querySelector(".hamburger-menu-wrapper.opened");
+        let menuTriggerElt = document.querySelector(".hamburger-menu-button[data-hamburger-menu-target=\"" + openedMenuElt.id + "\"]");
+        if (openedMenuElt !== null) {
+            openedMenuElt.classList.add("closed");
+            openedMenuElt.classList.remove("opened");
+            menuTriggerElt.classList.add("hamburger");
+            menuTriggerElt.classList.remove("close");
+            document.body.classList.remove("menu-opened");
+            giveTabulation(tabableElements);
+        }
+    }
+
+    function hamburgerMenuOpen (buttonElt) {
+        let menuElt = document.getElementById(buttonElt.dataset.hamburgerMenuTarget);
+        hamburgerMenuClose();
+        buttonElt.classList.add("close");
+        buttonElt.classList.remove("hamburger");
+        menuElt.classList.add("opened");
+        menuElt.classList.remove("closed");
+        document.body.classList.add("menu-opened");
+        removeTabulation(tabableElements);
+    }
+
+    let hamburgerButtonElts = document.getElementsByClassName("hamburger-menu-button");
+    let blurElt = document.getElementById("blur");
+    let contentElt = document.getElementById("content");
+
+    let tabableElements = getTabElements(contentElt);
+
+    for (let i = 0 ; i < hamburgerButtonElts.length ; i++) {
+        hamburgerButtonElts[i].addEventListener("click", function(e) {
+            let menuElt = document.getElementById(e.currentTarget);
+            hamburgerMenuToggle(menuElt);
+        });
+    }
+
+    window.addEventListener("keyup", function(e) {
+        if (e.key === "Escape") {
+            hamburgerMenuClose();
+        }
+    });
+
+    blurElt.addEventListener("click", function(e) {
+        hamburgerMenuClose();
+    })
+})();
+
+
+/*
 function hamburgerMenuAction(action) {
     hamburgerElt.focus();
     if (action === "hide" || action === "toggle" && document.body.classList.contains("opened")) {
@@ -40,3 +99,4 @@ window.addEventListener("keyup", function(e) {
 window.addEventListener("resize", function(e) {
     hamburgerMenuAction("hide");
 });
+ */
