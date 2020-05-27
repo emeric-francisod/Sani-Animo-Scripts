cebd20d6372436685b2ec2929f862275d4a4cf2a
emeric-francisod <emeric.francisod@gmail.com>
2020-05-27 16:03:11 +0200

    Nettoyage et isolation du code du popin

--- popin/script.js
+++ popin/script.js
@@ -1,46 +1,49 @@
-function openPopin( popinElt ) {
-    popinElt.classList.add( "opened" );
-    document.body.classList.add( "popin-opened" );
-
-    removeTabulation(tabableElements);
-}
-
-function closePopin() {
-    let openedPopinElt = document.querySelector(".popin.opened");
-    if (openedPopinElt !== null) {
-        openedPopinElt.classList.remove("opened");
-        document.body.classList.remove("popin-opened");
+(function() {
+    function openPopin(popinElt) {
+        closePopin();
+        popinElt.classList.add("opened");
+        document.body.classList.add("popin-opened");
+        removeTabulation(tabableElements);
     }
 
-    giveTabulation(tabableElements);
-}
-
-let popinOpenersElts = document.getElementsByClassName("popin-opener");
-let popinCloseButtonElts = document.getElementsByClassName("popin-close");
-let blurElt = document.getElementById("blur");
-let contentElt = document.getElementById("content");
-
-let tabableElements = getTabElements(contentElt);
-
-for (let i = 0 ; i < popinOpenersElts.length ; i++) {
-    popinOpenersElts[i].addEventListener("click", function(e) {
-        let popinElt = document.getElementById(e.currentTarget.dataset.popinTarget);
-        openPopin(popinElt);
-    });
-}
-
-for (let i = 0 ; i < popinCloseButtonElts.length ; i++) {
-    popinCloseButtonElts[i].addEventListener("click", function(e) {
-        closePopin();
-    });
-}
-
-window.addEventListener("keyup", function(e) {
-    if (e.key === "Escape" && document.body.classList.contains("popin-opened")) {
-        closePopin();
+    function closePopin() {
+        let openedPopinElt = document.querySelector(".popin.opened");
+        if (openedPopinElt !== null) {
+            openedPopinElt.classList.remove("opened");
+            document.body.classList.remove("popin-opened");
+            giveTabulation(tabableElements);
+        }
     }
-})
 
-blurElt.addEventListener("click", function(e) {
-    closePopin();
-})
+    let popinOpenersElts = document.getElementsByClassName("popin-opener");
+    let popinCloseButtonElts = document.getElementsByClassName("popin-close");
+    let blurElt = document.getElementById("blur");
+    let contentElt = document.getElementById("content");
+
+    let tabableElements = getTabElements(contentElt);
+
+    for (let i = 0 ; i < popinOpenersElts.length ; i++) {
+        popinOpenersElts[i].addEventListener("click", function(e) {
+            let popinElt = document.getElementById(e.currentTarget.dataset.popinTarget);
+            openPopin(popinElt);
+        });
+    }
+
+    for (let i = 0 ; i < popinCloseButtonElts.length ; i++) {
+        popinCloseButtonElts[i].addEventListener("click", function(e) {
+            closePopin();
+        });
+    }
+
+    window.addEventListener("keyup", function(e) {
+        if (e.key === "Escape") {
+            closePopin();
+        }
+    })
+
+    blurElt.addEventListener("click", function(e) {
+        closePopin();
+    })
+})();
+
+
