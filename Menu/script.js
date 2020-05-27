301c1f3aa9cbb236b419b1e4c8ab2aac9971929e
emeric-francisod <emeric.francisod@gmail.com>
2020-05-27 18:12:48 +0200

    Fermeture du menu et remise à 0 du menu hamburger au redimensionnement de la page

--- Menu/script.js
+++ Menu/script.js
@@ -1,4 +1,10 @@
 (function () {
+    function resetPage() {
+        hamburgerMenuClose();
+        hamburgerMenuElt.classList.remove("closed");
+        hamburgerButtonElt.classList.remove("hamburger");
+    }
+
     function hamburgerMenuToggle () {
         if (hamburgerMenuElt.classList.contains("opened")) {
             hamburgerMenuClose();
@@ -44,53 +50,12 @@
         }
     });
 
+    window.addEventListener("resize", function(e) {
+        resetPage();
+    });
+
     blurElt.addEventListener("click", function(e) {
         hamburgerMenuClose();
     })
+
 })();
-
-
-/*
-function hamburgerMenuAction(action) {
-    hamburgerElt.focus();
-    if (action === "hide" || action === "toggle" && document.body.classList.contains("opened")) {
-        document.body.classList.remove("opened");
-        document.body.classList.add("closed");
-        giveTabNavigation(bodyTabOrderElts);
-        removeTabNavigation(sidebarTabOrderElts);
-    } else if (action === "show" || action === "toggle" && !document.body.classList.contains("opened")) {
-        document.body.classList.add("opened");
-        document.body.classList.remove("closed");
-        removeTabNavigation(bodyTabOrderElts);
-        giveTabNavigation(sidebarTabOrderElts);
-    }
-}
-
-let hamburgerElt = document.getElementById("hamburger-button");
-let sidebarElt = document.getElementById("sidebar");
-let blurElt = document.getElementById("blur");
-let contentElt = document.getElementById("content");
-
-let bodyTabOrderElts = getTabElements(contentElt);
-let sidebarTabOrderElts = getTabElements(sidebarElt);
-
-removeTabNavigation(sidebarTabOrderElts);
-
-hamburgerElt.addEventListener("click", function(e){
-    hamburgerMenuAction("toggle");
-});
-
-blurElt.addEventListener("click", function(e) {
-    hamburgerMenuAction("hide");
-});
-
-window.addEventListener("keyup", function(e) {
-    if (e.key == "Escape" && this.document.body.classList.contains("opened")) {
-        hamburgerMenuAction("hide")
-    }
-});
-
-window.addEventListener("resize", function(e) {
-    hamburgerMenuAction("hide");
-});
- */
