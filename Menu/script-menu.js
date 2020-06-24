24bb2b0b4f73536de19933723c79832520edd69a
emeric-francisod <emeric.francisod@gmail.com>
2020-06-24 15:40:00 +0200

    Suppression du code inutile.

--- Menu/script-menu.js
+++ Menu/script-menu.js
@@ -31,69 +31,13 @@
         buttonElt.appendChild(svgElt);
     }
 
-    function resetPage() {
-        hamburgerMenuClose();
-        hamburgerMenuElt.classList.remove("closed");
-        hamburgerButtonElt.classList.remove("hamburger");
-    }
 
-    function hamburgerMenuToggle () {
-        if (hamburgerMenuElt.classList.contains("opened")) {
-            hamburgerMenuClose();
-        } else {
-            hamburgerMenuOpen();
-        }
-    }
-
-    function hamburgerMenuClose () {
-        if (hamburgerMenuElt.classList.contains("opened")) {
-            hamburgerMenuElt.classList.add("closed");
-            hamburgerMenuElt.classList.remove("opened");
-            hamburgerButtonElt.classList.add("hamburger");
-            hamburgerButtonElt.classList.remove("close");
-            document.body.classList.remove("menu-opened");
-            giveTabNavigation(tabableElements);
-            removeTabNavigation(hamburgerMenuTabableElements);
-        }
-    }
-
-    function hamburgerMenuOpen () {
-        hamburgerMenuElt.classList.remove("closed");
-        hamburgerMenuElt.classList.add("opened");
-        hamburgerButtonElt.classList.remove("hamburger");
-        hamburgerButtonElt.classList.add("close");
-        document.body.classList.add("menu-opened");
-        removeTabNavigation(tabableElements);
-        giveTabNavigation(hamburgerMenuTabableElements);
-    }
 
     let hamburgerButtonElt = document.getElementById("hamburger-menu-button");
     let hamburgerMenuElt = document.getElementById("hamburger-menu-wrapper");
-    let blurElt = document.getElementById("blur");
     let contentElt = document.getElementById("content");
 
     initializeWrapper(hamburgerMenuElt);
     initializeHamburgerButton(hamburgerButtonElt);
-
-    let tabableElements = getTabElements(contentElt);
-    let hamburgerMenuTabableElements = getTabElements(hamburgerMenuElt);
-
-    hamburgerButtonElt.addEventListener("click", function(e) {
-        hamburgerMenuToggle();
-    });
-
-    window.addEventListener("keyup", function(e) {
-        if (e.key === "Escape") {
-            hamburgerMenuClose();
-        }
-    });
-
-    window.addEventListener("resize", function(e) {
-        resetPage();
-    });
-
-    blurElt.addEventListener("click", function(e) {
-        hamburgerMenuClose();
-    })
 
 })();
