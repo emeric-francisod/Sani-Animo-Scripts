7a91d5dbb8ceffdade053c6dae277c779df72748
emeric-francisod <emeric.francisod@gmail.com>
2020-06-27 11:10:23 +0200

    Ajout de la fonction de récupération de données AJAX

---
+++ ajaxAPI/ajaxAPI.js
@@ -0,0 +1,18 @@
+function ajaxGet(url, callback) {
+    let request = new XMLHttpRequest();
+    request.open("GET", url);
+
+    request.addEventListener("load", function() {
+        if (request.status >= 200 && request.status < 400) {
+            callback(request.responseText);
+        } else {
+            console.error(request.status + " " + request.statusText + " " + url);
+        }
+    });
+
+    request.addEventListener("error", function() {
+        console.error("Erreur de réseau avec l'URL " + url);
+    });
+
+    request.send(null);
+}
