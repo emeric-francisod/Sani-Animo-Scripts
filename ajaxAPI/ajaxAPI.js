57be640503ce042fadb4deb1976570530431f5be
emeric-francisod <emeric.francisod@gmail.com>
2020-06-27 11:18:06 +0200

    Ajout de la fonction d'envoi de données AJAX

--- ajaxAPI/ajaxAPI.js
+++ ajaxAPI/ajaxAPI.js
@@ -16,3 +16,28 @@ function ajaxGet(url, callback) {
 
     request.send(null);
 }
+
+
+
+function ajaxPost(url, data, callback, isJson) {
+    let request = new XMLHttpRequest();
+    request.open("POST", url);
+
+    request.addEventListener("load", function() {
+        if (request.status >= 200 && request.status < 400) {
+            callback(request.responseText);
+        } else {
+            console.error(request.status + " " + request.statusText + " " + url);
+        }
+    });
+
+    request.addEventListener("error", function() {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+
+    if (isJson) {
+        request.setRequestHeader("Content-Type", "application/json");
+        data = JSON.stringify(data);
+    }
+    request.send(data);
+}
