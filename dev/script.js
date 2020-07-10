2a9a08a61f873a3bf6409bf61076ef9931665232
emeric-francisod <emeric.francisod@gmail.com>
2020-07-10 12:06:02 +0200

    Rotation des images dans les deux sens.

--- dev/script.js
+++ dev/script.js
@@ -1,20 +1,35 @@
 function rotateCarousel(carouselElt, forward = true) {
-    let currentImageElt = carouselElt.getElementsByClassName("sas-carousel-current-image");
-    for (let i = 0 ; i < currentImageElt.length ; i++) {
-        element = currentImageElt[i];
+    let currentImageElts = carouselElt.getElementsByClassName("sas-carousel-current-image");
+    for (let i = 0 ; i < currentImageElts.length ; i++) {
+        let currentImage = currentImageElts[i];
         if (forward) {
-            currentImageElt[i].classList.add("sas-carousel-prev-image");
-            currentImageElt[i].classList.remove("sas-carousel-current-image");
-            if (currentImageElt[i].previousElementSibling !== null) {
-                currentImageElt[i].previousElementSibling.classList.add("sas-carousel-prev-hidden");
-                currentImageElt[i].previousElementSibling.classList.remove("sas-carousel-prev-image");
+            currentImage.classList.remove("sas-carousel-current-image");
+            currentImage.classList.add("sas-carousel-prev-image");
+            if (currentImage.previousElementSibling !== null) {
+                currentImage.previousElementSibling.classList.remove("sas-carousel-prev-image");
+                currentImage.previousElementSibling.classList.add("sas-carousel-prev-hidden");
             }
-            if (currentImageElt[i].nextSibling !== null) {
-                currentImageElt[i].nextSibling.classList.add("sas-carousel-current-image");
-                currentImageElt[i].nextSibling.classList.remove("sas-carousel-next-image");
-                if (currentImageElt[i].nextSibling.nextSibling !== null) {
-                    currentImageElt[i].nextSibling.nextSibling.classList.add("sas-carousel-next-image");
-                    currentImageElt[i].nextSibling.nextSibling.classList.remove("sas-carousel-next-hidden");
+            if (currentImage.nextElementSibling !== null) {
+                currentImage.nextElementSibling.classList.remove("sas-carousel-next-image");
+                currentImage.nextElementSibling.classList.add("sas-carousel-current-image");
+                if (currentImage.nextElementSibling.nextElementSibling !== null) {
+                    currentImage.nextElementSibling.nextElementSibling.classList.remove("sas-carousel-next-hidden");
+                    currentImage.nextElementSibling.nextElementSibling.classList.add("sas-carousel-next-image");
+                }
+            }
+        } else {
+            currentImage.classList.remove("sas-carousel-current-image");
+            currentImage.classList.add("sas-carousel-next-image");
+            if (currentImage.nextElementSibling !== null) {
+                currentImage.nextElementSibling.classList.remove("sas-carousel-next-image");
+                currentImage.nextElementSibling.classList.add("sas-carousel-next-hidden");
+            }
+            if (currentImage.previousElementSibling !== null) {
+                currentImage.previousElementSibling.classList.remove("sas-carousel-prev-image");
+                currentImage.previousElementSibling.classList.add("sas-carousel-current-image");
+                if (currentImage.previousElementSibling.previousElementSibling !== null) {
+                    currentImage.previousElementSibling.previousElementSibling.classList.remove("sas-carousel-prev-hidden");
+                    currentImage.previousElementSibling.previousElementSibling.classList.add("sas-carousel-prev-image");
                 }
             }
         }
@@ -22,4 +37,3 @@ function rotateCarousel(carouselElt, forward = true) {
 }
 
 let carouselElt = document.getElementById("carousel");
-let element;
