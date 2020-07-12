6fbb61f659f793ab033a7296d785440d0d572c0f
emeric-francisod <emeric.francisod@gmail.com>
2020-07-12 23:21:41 +0200

    Indexation des images et rotation des indexs.

--- dev/script.js
+++ dev/script.js
@@ -1,49 +1,43 @@
-function rotateCarousel(carouselElt, forward = true) {
-    let currentImageElts = carouselElt.getElementsByClassName("sas-carousel-current-image");
-    for (let i = 0 ; i < currentImageElts.length ; i++) {
-        let currentImage = currentImageElts[i];
-        if (forward) {
-            currentImage.classList.remove("sas-carousel-current-image");
-            currentImage.classList.add("sas-carousel-prev-image");
-            if (currentImage.previousElementSibling !== null) {
-                currentImage.previousElementSibling.classList.remove("sas-carousel-prev-image");
-                currentImage.previousElementSibling.classList.add("sas-carousel-prev-hidden");
-            }
-            if (currentImage.nextElementSibling !== null) {
-                currentImage.nextElementSibling.classList.remove("sas-carousel-next-image");
-                currentImage.nextElementSibling.classList.add("sas-carousel-current-image");
-                if (currentImage.nextElementSibling.nextElementSibling !== null) {
-                    currentImage.nextElementSibling.nextElementSibling.classList.remove("sas-carousel-next-hidden");
-                    currentImage.nextElementSibling.nextElementSibling.classList.add("sas-carousel-next-image");
-                }
-            }
-        } else {
-            currentImage.classList.remove("sas-carousel-current-image");
-            currentImage.classList.add("sas-carousel-next-image");
-            if (currentImage.nextElementSibling !== null) {
-                currentImage.nextElementSibling.classList.remove("sas-carousel-next-image");
-                currentImage.nextElementSibling.classList.add("sas-carousel-next-hidden");
-            }
-            if (currentImage.previousElementSibling !== null) {
-                currentImage.previousElementSibling.classList.remove("sas-carousel-prev-image");
-                currentImage.previousElementSibling.classList.add("sas-carousel-current-image");
-                if (currentImage.previousElementSibling.previousElementSibling !== null) {
-                    currentImage.previousElementSibling.previousElementSibling.classList.remove("sas-carousel-prev-hidden");
-                    currentImage.previousElementSibling.previousElementSibling.classList.add("sas-carousel-prev-image");
-                }
-            }
-        }
+function initializeCarousel(carouselElt, imagesArray) {
+    let imagesElts = carouselElt.getElementsByTagName("img");
+    let imagesNb = imagesElts.length;
+
+    for (let i = 0 ; i < imagesNb ; i++) {
+        imagesArray.push(imagesElts[i]);
     }
+
+    updateIndex(imagesArray);
+}
+
+function updateIndex(imagesArray) {
+    let imagesNb = imagesArray.length;
+
+    for (let i = 0 ; i < imagesNb ; i++) {
+        imagesArray[i].dataset.sasCarouselIndex = i - Math.floor(imagesNb / 2);
+    }
+}
+
+function rotateCarousel(carouselImages, forward = true) {
+    if (forward) {
+        carouselImages.push(carouselImages.shift());
+    } else {
+        carouselImages.unshift(carouselImages.pop());
+    }
+
+    updateIndex(carouselImages);
 }
 
 let carouselElt = document.getElementById("carousel");
 let nextButtonElt = carouselElt.getElementsByClassName("sas-carousel-next-button")[0];
 let prevButtonElt = carouselElt.getElementsByClassName("sas-carousel-prev-button")[0];
 
+let images = new Array();
+initializeCarousel(carouselElt, images);
+
 nextButtonElt.addEventListener("click", function(e) {
-    rotateCarousel(carouselElt, true);
+    rotateCarousel(images, true);
 });
 
 prevButtonElt.addEventListener("click", function(e) {
-    rotateCarousel(carouselElt, false);
+    rotateCarousel(images, false);
 });
