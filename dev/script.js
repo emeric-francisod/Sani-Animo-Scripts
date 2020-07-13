df6f313768a1744940acdb3105358566ea6f73b0
emeric-francisod <emeric.francisod@gmail.com>
2020-07-13 23:04:15 +0200

    Préparation des tests pour les cas particuliers.

--- dev/script.js
+++ dev/script.js
@@ -1,3 +1,14 @@
 let carouselElt = document.getElementById("carousel");
-
 let carouselObj = new Carousel(carouselElt);
+
+
+let carousel2Elt = document.getElementById("carousel2");
+let carousel2Obj = new Carousel(carousel2Elt);
+
+
+let carousel3Elt = document.getElementById("carousel3");
+let carousel3Obj = new Carousel(carousel3Elt);
+
+
+let carousel4Elt = document.getElementById("carousel4");
+let carousel4Obj = new Carousel(carousel4Elt);
