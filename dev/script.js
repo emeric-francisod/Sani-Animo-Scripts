82af89294dc2d174754d27694bc89bfb99e66161
emeric-francisod <emeric.francisod@gmail.com>
2020-07-14 16:17:48 +0200

    Allège le fichier de test html.

--- dev/script.js
+++ dev/script.js
@@ -1,18 +1,2 @@
 let carouselElt = document.getElementById("carousel");
 let carouselObj = new Carousel(carouselElt);
-
-
-let carousel2Elt = document.getElementById("carousel2");
-let carousel2Obj = new Carousel(carousel2Elt);
-
-
-let carousel3Elt = document.getElementById("carousel3");
-let carousel3Obj = new Carousel(carousel3Elt);
-
-
-let carousel4Elt = document.getElementById("carousel4");
-let carousel4Obj = new Carousel(carousel4Elt);
-
-
-let carousel5Elt = document.getElementById("carousel5");
-let carousel5Obj = new Carousel(carousel5Elt);
